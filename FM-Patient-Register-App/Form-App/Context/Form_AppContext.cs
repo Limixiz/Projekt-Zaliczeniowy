57b5638ae122b831e94b84c67c6c65bed1d69a46
Limixiz <61080263+Limixiz@users.noreply.github.com>
2020-10-06 22:41:43 +0200

    NewPatientAddForm

---
+++ FM-Patient-Register-App/Form-App/Context/Form_AppContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Form_App.Context
+{
+    public class Form_AppContext : IdentityDbContext
+    {
+        public Form_AppContext(DbContextOptions<Form_AppContext> options) : base(options)
+        {
+
+        }
+    }
+}
