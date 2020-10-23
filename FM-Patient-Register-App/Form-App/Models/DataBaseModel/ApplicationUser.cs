2b6e1665ca95afd024caed4c06339be7af4123fd
Limixiz <61080263+Limixiz@users.noreply.github.com>
2020-10-24 00:28:59 +0200

    Revert "Revert "0.01""
    This reverts commit 9f2dfb0e3e49464a79dbc11d053ba02a924716d8.

---
+++ FM-Patient-Register-App/Form-App/Models/DataBaseModel/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Form_App.Models.DataBaseModel
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public ApplicationUser()
+        {
+        }
+
+        public ApplicationUser(string userName) : base(userName)
+        {
+        }
+
+        public List<Patient> Patients { get; set; }
+    }
+}
---
+++ FM-Patient-Register-App/Form-App/ViewModels/TherapyInfoViewModel/AddTherapyVewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Form_App.ViewModels.TherapyInfoViewModel
+{
+    public class AddTherapyVewModel
+    {
+        [Required(ErrorMessage = "Ta rubryka jest wymagana!")]
+        [Display(Name = "Wywiad")]
+        public string Review { get; set; }
+#nullable enable        
+        [Display(Name = "Dolegliwości")]
+        public string? Disorder { get; set; }
+        
+        [Display(Name = "Ruchomość")]
+        public string? RangeOfMotion { get; set; }
+        
+        [Display(Name = "Skala Vas")]
+        public string? VasScale { get; set; }
+        
+        [Display(Name = "Testy")]
+        public int Tests { get; set; }
+#nullable disable
+        [Required(ErrorMessage = "Ta rubryka jest wymagana!")]
+        [Display(Name = "Zastosowana Terapia")]
+        public string Therapy { get; set; }
+#nullable enable
+
+        [Display(Name = "Zalecenia")]
+        public string? Recommendation { get; set; }
+        [Display(Name = "Informacje dodatkowe")]
+        public string? AdisionalInfo { get; set; }
+#nullable disable
+    }
+}
